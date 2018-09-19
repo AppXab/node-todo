fb1c38e164c2d1345dca4177cc04a533612832a4
AppXab <ab@appxtechnologies.com>
2018-09-19 18:53:23 +0530

    add test for GET/todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -8,9 +8,19 @@ const {
   Todo
 } = require('./../models/todo');
 
+
+const todos = [{
+  text: 'test todo text'
+}, {
+  text: 'second test todo'
+}];
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos)
+  }).then(() => done());
 });
+
 
 describe('POST/todos', () => {
   it('should create new todo', (done) => {
@@ -29,7 +39,7 @@ describe('POST/todos', () => {
           return done(err);
         }
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(1);
+          expect(todos.length).toBe(3);
           expect(todos[0].text).toBe(text);
           done();
         }).catch((e) => done(e));
@@ -46,9 +56,21 @@ describe('POST/todos', () => {
           return done(err);
         }
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2); //no of input as constant todo has two elements
           done();
         }).catch((e) => done(e));
       })
+  })
+})
+
+describe('GET/todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   })
 })
