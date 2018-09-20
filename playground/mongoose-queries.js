bd3b6b934e14eb6b7fcc68d5f5e6425347286467
AppXab <ab@appxtechnologies.com>
2018-09-20 12:37:56 +0530

    add GET/todos/:id

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,14 +1,11 @@
-const {
-  ObjectID
-} = require('mongodb')
+const {  ObjectID} = require('mongodb')
 
-const {
-  mongoose
-} = require('./../server/db/mongoose');
+const {  mongoose} = require('./../server/db/mongoose');
 // const {  Todo} = require('./../server/models/todo');
 const {
   User
 } = require('./../server/models/user');
+
 
 var id = "5b9ba6d604071b09285d1cbb"; //5ba24c100d862c201407c71a-todo
 if (!ObjectID.isValid(id)) {
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {
+  ObjectID
+} = require('mongodb')
 var {
   mongoose
 } = require('./db/mongoose');
@@ -8,7 +11,7 @@ var {
   Todo
 } = require('./models/todo');
 var {
-  user
+  User
 } = require('./models/user');
 
 
@@ -32,14 +35,44 @@ app.post('/todos', (req, res) => { //todos is collection name
 });
 
 
-app.get('/todos', (req,res) => {
-  Todo.find().then((todos)=>{
-    res.send({todos});
-  },(e)=>{
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({
+      todos
+    });
+  }, (e) => {
     res.status(400).send(e);
   })
 
 })
+
+//api starts 
+//GET/todos/151314(id)
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(400).send();
+  }
+
+  Todo.findById(id).then((todo) => { //nothing is depend on this Users we can write any thing here
+    if (!todo) {
+      return res.status(400).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+
+
+});
+
+
+
+
+
+
+
+
 
 app.listen(3000, () => {
   console.log('started on port 3000')
