fd0e229ce90df2ac4423bd1d55ca31e25e2771a0
AppXab <ab@appxtechnologies.com>
2018-09-20 18:26:47 +0530

    Test the DELETE/todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -114,3 +114,41 @@ describe('GET/todos/:id', () => {
 
 
 })
+
+
+//api delete 
+describe('DELETE/todos/:id', () => {
+  it('should delete todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+    request(app)
+      .delete(`/todos/${hexId}`) //backtick
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        Todo.findById(hexId).then((todos) => {
+          expect(todos).toNotExist;
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+  it('should return 404 if todo not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+    request(app)
+      .get(`/todos/${hexId}`) //backtick
+      .expect(400)
+      .end(done)
+
+  });
+  it('should return 400 for non object ids', (done) => {
+    request(app)
+      .get('/todos/123abc') //backtick
+      .expect(400)
+      .end(done)
+  });
+
+})
