60a38979ab00e223a88b53f7fc0124e95f7d8303
AppXab <ab@appxtechnologies.com>
2018-09-21 10:29:53 +0530

    patch

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-
+//library require
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {
   ObjectID
 } = require('mongodb');
+
+//local require
 var {
   mongoose
 } = require('./db/mongoose');
@@ -85,6 +88,36 @@ app.delete('/todos/:id', (req, res) => {
     res.status(400).send();
   })
 });
+
+//add patch to api to add this  update method
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt= new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  Todo.findByIdAndUpdate(id, {
+    $set: body
+  }, {
+    new: true
+  }).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({
+      todo
+    });
+  }).catch((e) => {
+    res.status(404).send();
+  })
+})
 
 
 
