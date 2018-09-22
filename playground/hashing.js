17d3c271544aed64ab31b36d407b64bb23c107b2
AppXab <ab@appxtechnologies.com>
2018-09-22 18:36:45 +0530

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -2,18 +2,44 @@ const {
   SHA256
 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var password = 'abcd123';
 
-var data = {
-  id: 10
-};
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log('hash : ' + hash);
+  })
+})
 
-var token = jwt.sign(data, 'abcd123');
-console.log(token);
-//it will result eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTUzNzYwNDk4MH0.RKNszI_Sdds3mRTnDLmJj4RcFNiN0pu8b6ShNl29nb8
-//here 1st part is header ,2nd and 3rd are payload our info,last part is hash
 
-var decoded=jwt.verify(token,'abcd123');
-console.log('decoded',decoded);
+//to check above value with its hashed value
+var hashedPassword = '$2a$10$7GFu9EJ61JLtLISrUtyEluWDQ0GK3RHgBEZ9/8CmlZQEMlBe8qdXa';
+
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+})
+
+
+
+
+
+
+
+
+
+// var data = {
+//   id: 10
+// };
+
+// var token = jwt.sign(data, 'abcd123');
+// console.log(token);
+
+// //it will result eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTUzNzYwNDk4MH0.RKNszI_Sdds3mRTnDLmJj4RcFNiN0pu8b6ShNl29nb8
+// //here 1st part is header ,2nd and 3rd are payload our info,last part is hash
+
+// var decoded=jwt.verify(token,'abcd123');
+// console.log('decoded',decoded);
 
 
 // var message = 'i am number 4';
